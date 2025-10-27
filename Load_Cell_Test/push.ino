4c3ebd40dcc4d1a6f209fc453cd6bc7080b16ae8
LianMollick <78555845+MevrickNeal@users.noreply.github.com>
2025-10-28 01:52:42 +0600

    Update push.ino

--- Load_Cell_Test/push.ino
+++ Load_Cell_Test/push.ino
@@ -1,24 +1,27 @@
 byte push() {
   byte count = 0;
 again:
-  int t = 0;
+  int timer = 0;
   if (digitalRead(button)) {
+
     delay(100);  //debounce delay
+    digitalWrite(led, 0);
     while (digitalRead(button)) {
       delay(50);
-      t++;
+      timer++;
     }
-
-    if (t) {
+    digitalWrite(led, 1);
+    if (timer) {
       count++;
       timer = 0;
       while (!digitalRead(button)) {
         delay(1);
-        t++;
-        if (t > 1000) return count;
+        timer++;
+        if (timer > 1000) return count;
       }
       goto again;
     }
   }
+  digitalWrite(led , 1);
   return count;
 }
